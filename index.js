2a8bb71126a0beb15c668798bcc29e2c528cbc3d
sammio2 <sammio2@gmail.com>
2017-04-27 15:45:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ restService.post('/echo', function(req, res) {
     
     //var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
     
-    var name = req.body.result.parameters.echoText
+    var thing = req.body.result.parameters.echoText
     var city = req.body.result.parameters.geoCity
     
     var speech = `Hello ${name}, how's ${city} treating you?`;
-    
+       
     //var speech = "Hello " += name +=" from " += city += ", How are you?"
     //var speech = "Hello World ALALALA"
     
@@ -29,6 +29,26 @@ restService.post('/echo', function(req, res) {
         source: 'webhook-echo-sample'
     });
 });
+
+restService.post('/search', function(req, res) {
+    
+    //var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
+    
+    var thing = req.body.result.parameters.yellThing
+    var city = req.body.result.parameters.yellLocation
+    
+    var speech = `Okay, here's what I found... https://www.yell.com/ucs/UcsSearchAction.do?keywords=${thing}&location=${city}`;
+       
+    //var speech = "Hello " += name +=" from " += city += ", How are you?"
+    //var speech = "Hello World ALALALA"
+    
+    return res.json({
+        speech: speech,
+        displayText: speech,
+        source: 'webhook-echo-sample'
+    });
+});
+
 
 restService.listen((process.env.PORT || 8000), function() {
     console.log("Server up and listening");
